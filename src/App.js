f28db3ce023467daf88549e60306566f64c745e8
dhurvthiside <dhruvchauhan6488@gmail.com>
2025-03-04 18:20:10 +0530

    Commit 3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import NamePage from "./Pages/NamePage";
+import ScrollToTop from "./Pages/ScrollToTop"; // Import the component
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:name" element={<NamePage />} /> {/* Single dynamic route */}
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:name" element={<NamePage />} />
+      </Routes>
+    </>
   );
 };
 
